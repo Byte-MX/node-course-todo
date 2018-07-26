5cbf2e869a97c534fc78ae40aa3654341e8a1aff
Alberto <ing_aperezr@yahoo.com>
2018-07-26 16:09:31 -0500

    Completado listar (sin ayuda del curso) y añadir.

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
+// Require de la biblioteca de colores (https://www.npmjs.com/package/colors)
+const colors = require('colors/safe');
+
 const argv = require('./config/yargs').argv;
 
-//console.log(argv);
+const porHacer = require('./por-hacer/por-hacer');
+
 
 let comando = argv._[0];
+//console.log(argv);
 
 switch (comando) {
 
     case 'crear':
-        console.log("Crear pendiente");
+        let tarea = porHacer.crear(argv.descripcion);
         break;
     case 'listar':
-        console.log("Mostrar todas las tareas pendientes");
+        let cuales = 'Pendientes'; //default
+        let pendientes = argv.pendientes;
+        //console.log("Pendientes: " + pendientes);
+        let completadas = argv.completadas;
+        //console.log('Completadas: ' + completadas);
+        let todas = argv.todas;
+        //console.log(`Todas: ${todas}`);
+        if (todas || (pendientes && completadas)) {
+            cuales = 'Todas';
+        } else if (completadas && !pendientes) {
+            cuales = 'Completadas';
+        }
+        //console.log(`Por mostrar: ${cuales}`);
+        let listado = porHacer.getListado(cuales);
+        console.log(colors.cyan("\n========== Lista de tareas ==========\n"));
+        if (listado.length == 0) {
+            console.log(colors.cyan("No hay tareas pendientes."));
+        } else {
+            for (let tarea of listado) {
+                let completado = colors.red('Pendiente');
+                if (tarea.completado) {
+                    completado = colors.green('Completada');
+                }
+                console.log(` - ${tarea.descripcion}: \t` + completado);
+            }
+        }
+        console.log(colors.cyan("\n=====================================\n"));
         break;
     case 'actualizar':
         console.log('Actualiza una tarea pendiente');
--- por-hacer/por-hacer.js
+++ por-hacer/por-hacer.js
@@ -1,19 +1,74 @@
 // Require del FileSystem (https://nodejs.org/dist/latest-v10.x/docs/api/fs.html#fs_file_system)
 const fs = require('fs');
-
+// Require de la biblioteca de colores (https://www.npmjs.com/package/colors)
+const colors = require('colors/safe');
 
 let listadoPendientes = [];
 
+const guardarDB = () => {
+    return new Promise((reject, resolve) => {
+        let data = JSON.stringify(listadoPendientes);
+        fs.writeFile('db/data.json', data, (err) => {
+            if (err) throw new Error('La lista de actividades no se pudo guardar', err); //reject(err); 
+            else
+                resolve('Actividad guardada en base de datos.');
+        });
+    });
+};
+
+const cargarDB = () => {
+    try {
+        listadoPendientes = require('../db/data.json');
+    } catch (error) {
+        listadoPendientes = [];
+    }
+
+    //console.log(listadoPendientes);
+};
+
+const getListado = (cuales) => {
+    cargarDB();
+    let listadoAMostrar = [];
+    switch (cuales) {
+        case 'Todas':
+            for (let tarea of listadoPendientes) {
+                listadoAMostrar.push(tarea);
+            }
+            break;
+        case 'Completadas':
+            for (let tarea of listadoPendientes) {
+                if (tarea.completado) {
+                    listadoAMostrar.push(tarea);
+                }
+            }
+            break;
+        default:
+            for (let tarea of listadoPendientes) {
+                if (!tarea.completado) {
+                    listadoAMostrar.push(tarea);
+                }
+            }
+            break;
+    }
+    return listadoAMostrar;
+};
+
 const crear = (descripcion) => {
+    cargarDB();
+
     let pendiente = {
         descripcion,
         completado: false
     };
 
     listadoPendientes.push(pendiente);
+    guardarDB()
+        //.then(archivo => console.log(`Actividad: ${pendiente} guardada en archivo: `, colors.green(archivo)))
+        .catch(e => console.log(e));
     return pendiente;
 };
 
 module.exports = {
-    crear
+    crear,
+    getListado
 };
